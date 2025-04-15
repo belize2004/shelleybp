63118dfe53d02aad9bba8e0a443f9e17c752a1db
Adnan Saleem <adnan13893@gmail.com>
2025-04-15 21:14:15 +0500

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,90 +1,90 @@
-import type { MetadataRoute } from "next";
+import type {MetadataRoute} from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.shelleyandblaine.com",
+      url: 'https://www.shelleybphotography.com',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
+      changeFrequency: 'daily',
+      priority: 1
     },
     {
-      url: "https://www.shelleyandblaine.com/interior-design",
+      url: 'https://www.shelleybphotography.com/interior-design',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/real-estate",
+      url: 'https://www.shelleybphotography.com/real-estate',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/short-term-rental",
+      url: 'https://www.shelleybphotography.com/short-term-rental',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/family-portrait",
+      url: 'https://www.shelleybphotography.com/family-portrait',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/engagement-sessions",
+      url: 'https://www.shelleybphotography.com/engagement-sessions',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/senior-portraits",
+      url: 'https://www.shelleybphotography.com/senior-portraits',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     // {
-    //   url: "https://www.shelleyandblaine.com/micro-wedding",
+    //   url: "https://www.shelleybphotography.com/micro-wedding",
     //   lastModified: new Date(),
     //   changeFrequency: "daily",
     //   priority: 0.9,
     // },
     {
-      url: "https://www.shelleyandblaine.com/wall-art",
+      url: 'https://www.shelleybphotography.com/wall-art',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/website-design-service",
+      url: 'https://www.shelleybphotography.com/website-design-service',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/contact",
+      url: 'https://www.shelleybphotography.com/contact',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/about",
+      url: 'https://www.shelleybphotography.com/about',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/press",
+      url: 'https://www.shelleybphotography.com/press',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
+      changeFrequency: 'daily',
+      priority: 0.9
     },
     {
-      url: "https://www.shelleyandblaine.com/blog",
+      url: 'https://www.shelleybphotography.com/blog',
       lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 0.9,
-    },
-  ];
+      changeFrequency: 'daily',
+      priority: 0.9
+    }
+  ]
 }
